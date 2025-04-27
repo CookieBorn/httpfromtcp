8b724653bb23c9556aed93e80a8a63bd01a1e6cc
CookieBorn <robanikin@hotmail.com>
2025-04-28 06:45:30 +1000

    B: Newline print

--- main.go
+++ main.go
@@ -2,8 +2,35 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Print("I hope I get the job!\n")
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		fmt.Errorf("Open error: %v\n", err)
+		return
+	}
+	b := make([]byte, 8)
+	Line := []string{}
+	for 1 > 0 {
+		_, err := file.Read(b)
+		if err != nil {
+
+			fmt.Errorf("Open error: %v\n", err)
+			return
+		}
+		splitB := strings.Split(string(b), "\n")
+		if len(splitB) == 1 {
+			Line = append(Line, splitB[0])
+		} else {
+			Line = append(Line, splitB[0])
+			joinLine := strings.Join(Line, "")
+			fmt.Printf("read: %s\n", joinLine)
+			Line = []string{}
+			Line = append(Line, splitB[1])
+		}
+	}
+
 }
